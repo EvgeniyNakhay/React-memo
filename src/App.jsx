2563eefd89f84555bce66ccf62a5f8dc565c83c8
EvgeniyNakhay <qwert57894@gmail.com>
2025-07-29 18:47:43 +0300

    Created TodoList folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 import "./App.css";
-import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList/TodoList";
 import GoodsBasket from "./components/GoodsBasket";
 import UserProfile from "./components/UserProfile/UserProfile";
 function App() {
   return (
     <>
-      {/* <GoodsBasket /> */}
-      <TodoList />
+      <GoodsBasket />
+      {/* <TodoList /> */}
       {/* <UserProfile /> */}
     </>
   );
